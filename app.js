42e324c56f07a8daeeb153c4a8edcf5976b283eb
Raja Sahu <rajasahu001@gmail.com>
2018-12-25 21:00:26 +0530

    changed app.js

--- app.js
+++ app.js
@@ -9,5 +9,32 @@ GAME RULES:
 
 */
 
-var dice = 12;
-console.log(dice);
+var scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	
+	//1. random number
+	 var dice = Math.floor(Math.random() * 6) + 1;
+
+	//2. display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+	//3.update the round score If the rolled number was NOT a 1
+
+});
+
+
